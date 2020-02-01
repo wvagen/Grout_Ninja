eff58eacd3c771f27668dc421843c5e268748ad5
Mouadh Mkadmi <wvagen@gmail.com>
2020-02-01 05:14:11 +0100

    Version 1.0

---
+++ Assets/Scripts/GroutLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroutLogic : MonoBehaviour
+{
+
+    Transform holePos = null;
+
+    float distance;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name);
+        holePos = col.transform;
+    }
+
+    void Update()
+    {
+        if (holePos != null)
+        {
+            QuickMaths();
+        }
+    }
+
+    void QuickMaths()
+    {
+        distance = Vector2.Distance(transform.position, holePos.position);
+        if (distance < (holePos.transform.localScale.x / 2) * Mathf.Sqrt(2))
+        {
+            Debug.Log("Covered");
+        }
+    }
+
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+
+    public GameObject smallGrout;
+
+    GameObject lastGroutCreated;
+    Vector2 mousePos;
+    bool isMouseDown = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) OnDown();
+        if (Input.GetMouseButtonUp(0)) OnUp();
+        if (isMouseDown) followMyMouse();
+    }
+
+    void OnDown()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastGroutCreated = Instantiate(smallGrout, mousePos, Quaternion.identity);
+        isMouseDown = true;
+    }
+
+    void OnUp()
+    {
+        lastGroutCreated = null;
+        isMouseDown = false;
+    }
+
+    void followMyMouse()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lastGroutCreated.transform.position = mousePos;
+    }
+
+}
